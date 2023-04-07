d46ed747e3587bd1c6e33b68a3e9a6e68abbbf91
Obieda Akilan <obieda.jehad@gmail.com>
2023-04-07 14:18:36 +0300

    Added navbar section and its style

--- Frontend/src/components/navbar/Navbar.jsx
+++ Frontend/src/components/navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
-import userImage from '../../assets/login.jpeg'
+import profileImage from '../../assets/login.jpeg';
 import './navbar.scss';
 const Navbar = () => {
   return (
@@ -30,11 +30,10 @@ const Navbar = () => {
         <PersonOutlineOutlinedIcon />
         <EmailOutlinedIcon />
         <NotificationsNoneOutlinedIcon />
-        <div className="social__nav-user">
-           <img src={userImage} alt="" />
-        <span>Oubaida</span>
+        <div className='social__nav-user'>
+          <img src={profileImage} alt='profileImage' />
+          <span>Oubaida</span>
         </div>
-       
       </div>
     </nav>
   );
