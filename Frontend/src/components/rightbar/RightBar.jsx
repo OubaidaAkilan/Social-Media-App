e65ac540bf95dcfa72a88677ca071fc7614983ae
Obieda Akilan <obieda.jehad@gmail.com>
2023-05-22 21:37:26 +0300

    still working on the first section inside right-bar

--- Frontend/src/components/rightbar/RightBar.jsx
+++ Frontend/src/components/rightbar/RightBar.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import './rightBar.scss';
+import profileImage from '../../assets/login.jpeg';
+import { blue } from '@mui/material/colors';
 
 const RightBar = () => {
-  return <section className='social__rightBar'>Right Bar</section>;
+  return (
+    <section className='social__rightBar'>
+      <div className='social__rightBar-container'>
+        <div className='item'>
+          <span>Suggestion for you</span>
+          <div className='user'>
+            <div className='userInfo'>
+              <img src={profileImage} alt='userImge' />
+              <span>Jane Doe</span>
+            </div>
+            <div className='buttons'>
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default RightBar;
