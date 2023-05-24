95077d5a357de1396eddd7344f95677886c2fc81
Obieda Akilan <obieda.jehad@gmail.com>
2023-05-24 11:25:47 +0300

    Create user story section with its own style

--- Frontend/src/pages/home/Home.jsx
+++ Frontend/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './home.scss';
+import Stories from '../../components/stories/Stories';
 const Home = () => {
   return (
     <>
       <section className='social__home'>
+        <Stories />
         <Link to='/register'>
           <button>Home</button>
         </Link>
