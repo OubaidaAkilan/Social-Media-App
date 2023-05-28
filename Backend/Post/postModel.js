c1b839c25fd0779e138d5fa936883d60a74d6d8f
Obieda Akilan <obieda.jehad@gmail.com>
2023-05-28 13:41:21 +0300

    Create the Model of post and finished cascade

---
+++ Backend/Post/postModel.js
@@ -0,0 +1,33 @@
+'use strict';
+const mongoose = require('mongoose');
+const commentModel = require('../Comment/commentModel');
+
+const postSchema = new mongoose.Schema(
+  {
+    desc: {
+      type: String,
+      required: [true, 'Description of post is required'],
+    },
+    imgPost: {
+      type: String,
+      default: null,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'User of post is required'],
+    },
+  },
+  { timestamps: true }
+);
+
+// Pre-remove middleware for the psotSchema
+postSchema.pre('remove', async function (next) {
+  // Remove all comments associated with the post being removed
+  await commentModel.deleteMany({ post: this._id });
+  next();
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
