f8fe7cedb6bc968765e8a6096b8f8c119ca74160
Obieda Akilan <obieda.jehad@gmail.com>
2023-05-28 13:42:57 +0300

    Fix the dbConnection function to be async function

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,8 +1,8 @@
 'use strict';
 const mongoose = require('mongoose');
 
-const dbConnection = () => {
-  mongoose
+const dbConnection = async () => {
+  await mongoose
     .connect(process.env.DB_URI, { dbName: process.env.DB_NAME })
     .then((conn) => {
       console.log('Database connected :' + conn.connection.host);
