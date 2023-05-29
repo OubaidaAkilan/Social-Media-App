8c9e534d8dfff689c0165efa5701363910c5c53d
Obieda Akilan <obieda.jehad@gmail.com>
2023-05-29 09:20:36 +0300

    Account route file with end point's logging

---
+++ Backend/Account/accountRoutes.js
@@ -0,0 +1,10 @@
+'use strict';
+const { express } = require('../ourPackages.js');
+const { register } = require('./accountService.js');
+
+
+const router =express.Router();
+
+router.post('/register' , register);
+
+module.exports =router;
