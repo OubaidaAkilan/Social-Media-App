de63da80188621d546550c77460182f65b5b925b
Obieda Akilan <obieda.jehad@gmail.com>
2023-06-14 09:18:39 +0300

    Added share component and its own style

--- Frontend/src/pages/home/Home.jsx
+++ Frontend/src/pages/home/Home.jsx
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom';
 import './home.scss';
 import Stories from '../../components/stories/Stories';
 import Posts from '../../components/posts/Posts';
+import Share from '../../components/share/Share';
 
 const Home = () => {
   return (
     <>
       <section className='social__home'>
         <Stories />
+        <Share />
         <Posts />
         <Link to='/register'>
           <button>Home</button>
