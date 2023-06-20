1e31a65c73070b67f8a975aef3e966717ba99fc6
Obieda Akilan <obieda.jehad@gmail.com>
2023-06-20 10:12:18 +0300

    Implemented "follow/unfollow" functionality and refactor the services of follower

--- Frontend/src/pages/profile/Profile.jsx
+++ Frontend/src/pages/profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import './profile.scss';
 import FacebookTwoToneIcon from '@mui/icons-material/FacebookTwoTone';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
@@ -9,22 +9,31 @@ import PlaceIcon from '@mui/icons-material/Place';
 import LanguageIcon from '@mui/icons-material/Language';
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import Posts from '../../components/posts/Posts';
 import AxiosInstance from '../../api/AxiosInstance';
-import { useQuery } from 'react-query';
+import { useQuery, useQueryClient, useMutation } from 'react-query';
 import { useParams } from 'react-router-dom';
-// import { Cookies } from 'react-cookie';
+import { Cookies } from 'react-cookie';
+import { AuthContext } from '../../context/AuthContext';
 
 const Profile = () => {
-  // const cookies = new Cookies();
-  // const token = cookies.get('accessToken');
+  const cookies = new Cookies();
+  const token = cookies.get('accessToken');
+  const { currentUser } = useContext(AuthContext);
+
+  const [stateFollow, setStateFollow] = useState(null);
+
+  // Access the client
+  const queryClient = useQueryClient();
+
+  //======== User
 
   const { userId } = useParams();
+
+  const [proccess, setProccess] = useState(false);
 
   const fetchUser = async () => {
     try {
       const res = await AxiosInstance.get(`/users/${userId}`);
-      console.log('res.data.data >>>', res.data.data);
       return res.data.data;
     } catch (error) {
       throw new Error('Failed to fetch a user');
@@ -32,6 +41,93 @@ const Profile = () => {
   };
 
   const { data: user, error, isError, isLoading } = useQuery('user', fetchUser);
+
+  //======== Follower
+  const fetchFollowers = async () => {
+    try {
+      const res = await AxiosInstance.get(`/follower/${userId}/followers`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.log(res.data.data);
+      setStateFollow(res.data.data.includes(currentUser._id));
+      return res.data.data;
+    } catch (error) {
+      throw new Error('Failed to fetch a user');
+    }
+  };
+
+  const {
+    data: followers,
+    errorFollowers,
+    isErrorFollowers,
+    isLoadingFollowers,
+  } = useQuery('followers', fetchFollowers);
+
+  const handleFollow = async () => {
+    try {
+      await AxiosInstance.post(
+        `/follower/${userId}`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      setProccess(false);
+    } catch (error) {
+      throw new Error('Failed to follow');
+    }
+  };
+
+  const handleUnFollow = async () => {
+    try {
+      await AxiosInstance.delete(`/follower/${userId}/unfollow`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setProccess(false);
+    } catch (error) {
+      throw new Error('Failed to follow');
+    }
+  };
+
+  // Mutations
+  const mutation = useMutation(
+    (stateFollower) => {
+      if (stateFollower) return handleUnFollow();
+      return handleFollow();
+    },
+
+    {
+      onSuccess: () => {
+        // Invalidate and refetch
+        queryClient.invalidateQueries(['followers']);
+      },
+    }
+  );
+
+  const handleFollower = async (e) => {
+    e.preventDefault();
+    setProccess(true);
+
+    try {
+      mutation.mutate(followers.includes(currentUser._id));
+    } catch (error) {
+      console.error(error);
+      // Handle the error appropriately
+    }
+  };
+
+  if (isLoadingFollowers) return 'Loading follower...';
+
+  if (isErrorFollowers)
+    return 'An error has occurred: ' + errorFollowers.message;
 
   if (isLoading) return 'Loading...';
 
@@ -87,11 +183,19 @@ const Profile = () => {
               <span>{user?.email}</span>
             </div>
           </div>
-          <button>follow</button>
+          {stateFollow ? (
+            <button onClick={handleFollower} disabled={proccess}>
+              {proccess ? 'proccess ...' : 'unfollwow'}
+            </button>
+          ) : (
+            <button onClick={handleFollower} disabled={proccess}>
+              {proccess ? 'proccess ...' : 'follow'}
+            </button>
+          )}
         </div>
         <div className='right'>
-          <EmailOutlinedIcon />
-          <MoreVertIcon />
+          <EmailOutlinedIcon  />
+          <MoreVertIcon  />
         </div>
       </div>
     </section>
